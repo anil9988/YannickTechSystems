4df42fa435bbea91a285852b84e7699ed78c00e9
anil9988 <31654106+anil9988@users.noreply.github.com>
2017-09-05 19:45:13 +0530

    Initial commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(YannickTechSystems.Startup))]
+namespace YannickTechSystems
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
