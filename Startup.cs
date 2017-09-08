31400d6baf5986f4800f6f35b7488d7b07cf76ad
anil9988 <31654106+anil9988@users.noreply.github.com>
2017-09-08 17:12:19 +0530

    commit

--- Startup.cs
+++ Startup.cs
@@ -8,7 +8,7 @@ namespace YannickTechSystems
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
+           // ConfigureAuth(app);
         }
     }
 }
